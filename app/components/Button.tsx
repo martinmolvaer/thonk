5bbc7a47dedc477bc1958dfef3eb6f4afb24621a
martinmolvaer <martin.molvaer@gmail.com>
2025-04-24 11:33:39 +0200

    Refactor Button component and enhance Home page with sound functionality

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 
 interface ButtonProps {
   name: string;
-  onClick?: () => void;
+  color?: string;
+  id: number;
+  keyToPress: string;
+  onClick: () => void;
 }
 
-function Button({ name, onClick }: ButtonProps) {
+function Button({ name, onClick, color, keyToPress, id }: ButtonProps) {
   return (
-    <button
+    <div
+      id={String(id)}
       onClick={onClick}
-      className="group font-mono relative inline-flex size-16 items-center justify-center overflow-hidden rounded-md border border-neutral-300 bg-[#F1F1EC] px-6 text-xs font-bold text-neutral-600 transition-all [box-shadow:0px_5px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none"
+      className="group font-mono select-none  relative flex flex-col jus size-22 items-center justify-center overflow-hidden rounded-md border border-neutral-300 bg-[#F4F7FE] px-6 p-2 text-xs font-semibold text-neutral-600 transition-all [box-shadow:0px_8px_1px_#D9D9E6] active:translate-y-[4px] active:shadow-none"
     >
-      {name}
-    </button>
+      <div
+        className="absolute left-2 top-2 rounded-full size-3 mb-2 border border-neutral-300 transition-all group-hover:scale-110 group-active:scale-120"
+        style={{ backgroundColor: color }}
+      ></div>
+      <p className="text-neutral-400 absolute right-2 top-2 ">{keyToPress}</p>
+      <p className="text-center mt-2">{name}</p>
+    </div>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,72 @@
 'use client';
 import useSound from 'use-sound';
 import Button from './components/Button';
+import { useEffect } from 'react';
+import Link from 'next/link';
+
+const switches = [
+  {
+    id: 1,
+    name: 'Flurples Cardboard',
+    color: '#AD8762',
+    sound: '/cardboard.mp3',
+  },
+  {
+    id: 2,
+    name: 'NK Creams',
+    color: '#FFFFFD',
+    sound: '/creams.mp3',
+  },
+  {
+    id: 3,
+    name: 'Crystal Purple',
+    color: '#A7A2E5',
+    sound: '/crystalpurple.mp3',
+  },
+  {
+    id: 4,
+    name: 'Japanese Black',
+    color: '#000',
+    sound: '/japaneseblack.mp3',
+  },
+  {
+    id: 5,
+    name: 'Milky Yellow',
+    color: '#FFFFC5',
+    sound: '/milkyyellow.mp3',
+  },
+];
 
 export default function Home() {
-  const [playFlurples] = useSound('/flurples.mp3', {
-    volume: 0.5,
-  });
-  const [playCreams] = useSound('/creams.mp3', {
-    volume: 0.5,
-  });
-  const [cystalPurple] = useSound('/crystalpurple.mp3', {
-    volume: 0.5,
-  });
-  const [japaneseBlack] = useSound('/japaneseblack.mp3', {
-    volume: 0.5,
-  });
+  const playSound = (sound: string) => {
+    const audio = new Audio(sound);
+    audio.volume = 0.5;
+    audio.play();
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#d0d0d0] p-12 sm:p-24">
       <p className="absolute top-4 left-4 font-mono">clackers</p>
-      <div className="flex gap-4 items-center justify-center">
-        <Button name="Flurple" onClick={playFlurples} />
-        <Button name="Creams" onClick={playCreams} />
-        <Button name="Crystal Purple" onClick={cystalPurple} />
-        <Button name="Japanese Black" onClick={japaneseBlack} />
+      <div className="flex gap-1 flex-wrap items-center justify-center p-4 bg-[#999999] rounded-3xl shadow-xl border-10 border-[#F4F7FE]">
+        {switches.map((switchItem) => (
+          <Button
+            key={switchItem.id}
+            id={switchItem.id}
+            name={switchItem.name}
+            color={switchItem.color}
+            keyToPress={`#${switchItem.id}`}
+            onClick={() => playSound(switchItem.sound)}
+          />
+        ))}
       </div>
+      <Link
+        target="_blank"
+        rel="non-refferer"
+        href={'http://molvaer.no'}
+        className="absolute bottom-2 right-2 font-mono uppercase font-sm font-bold"
+      >
+        Made by Mart
+      </Link>
     </div>
   );
 }
